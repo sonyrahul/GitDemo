8064a8d4550b57c3bace75a082ca9be18ac75197
rahul <rahulsony23456@gmail.com>
2022-11-26 22:06:36 +0530

    My First Commit

---
+++ src/selenium/Introduction.java
@@ -0,0 +1,30 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Introduction {
+	WebDriver driver;
+	@BeforeMethod
+	public void chrome()
+	{
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+	}
+	@Test
+	public void getdata() {
+		driver.get("https://rahulshettyacademy.com/client");
+		driver.manage().window().maximize();
+
+	}
+	
+	@AfterMethod
+	public void tearDown()
+	{
+		driver.close();
+	}
+}
---
+++ src/selenium/google.java
@@ -0,0 +1,18 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class google {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/client");
+
+	}
+
+}
